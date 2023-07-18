9adae7934f3c8cdd7958a82562a6cf2e9836dd72
Jans Johnson <jaansjohnson@gmail.com>
2023-07-18 11:08:24 +0530

    Added buttons to swipe left and right

--- App.tsx
+++ App.tsx
@@ -1,19 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import SwipeCard from './SwipeCard';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <SwipeCard/>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
--- SwipeCard.tsx
+++ SwipeCard.tsx
@@ -1,109 +1,122 @@
+import React, { useRef } from "react";
 import Swiper from "react-native-deck-swiper";
 import { Button, StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
 
 const SwipeCard = () => {
+    const swiperRef = useRef<Swiper<string> | null>(null);
+
+  const swipeLeft = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swipeLeft();
+    }
+  };
+
+  const swipeRight = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swipeRight();
+    }
+  };
+
   return (
-        <Swiper
-            cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
-            verticalSwipe={false}
-            renderCard={(card) => {
-                return (
-                    <View style={styles.card}>
-                        <Text style={styles.text}>{card}</Text>
-                    </View>
-                )
-            }}
-            onSwiped={(cardIndex) => {console.log(cardIndex)}}
-            onSwipedAll={() => {console.log('onSwipedAll')}}
-            onSwiping={(prop)=>{
-                
-            }}
-            cardIndex={0}
-            backgroundColor={'#4FD0E9'}
-            stackSize= {3}
-            stackSeparation={10}
-            overlayLabels={{
-                left: {
-                  title: 'NOPE',
-                  style: {
-                    label: {
-                      backgroundColor: 'red',
-                      borderColor: 'black',
-                      color: 'white',
-                      borderWidth: 1
-                    },
-                    wrapper: {
-                      flexDirection: 'column',
-                      alignItems: 'flex-end',
-                      justifyContent: 'flex-start',
-                      marginTop: 30,
-                      marginLeft: -30
-                    }
-                  }
-                },
-                right: {
-                  title: 'LIKE',
-                  style: {
-                    label: {
-                      backgroundColor: 'green',
-                      borderColor: 'black',
-                      color: 'white',
-                      borderWidth: 1
-                    },
-                    wrapper: {
-                      flexDirection: 'column',
-                      alignItems: 'flex-start',
-                      justifyContent: 'flex-start',
-                      marginTop: 30,
-                      marginLeft: 30
-                    }
-                  }
-                },
-              }}
-              animateOverlayLabelsOpacity
-              animateCardOpacity
-              swipeBackCard
-            />
+    <View style={styles.container}>
+      <Swiper
+        ref={swiperRef}
+        cards={["DO", "MORE", "OF", "WHAT", "MAKES", "YOU", "HAPPY"]}
+        verticalSwipe={false}
+        renderCard={(card) => {
+          return (
+            <View style={styles.card}>
+              <Text style={styles.text}>{card}</Text>
+            </View>
+          );
+        }}
+        onSwiped={(cardIndex) => {
+          console.log(cardIndex);
+        }}
+        onSwipedAll={() => {
+          console.log("onSwipedAll");
+        }}
+        onSwiping={(prop) => {}}
+        cardIndex={0}
+        backgroundColor={"#4FD0E9"}
+        stackSize={3}
+        stackSeparation={10}
+        overlayLabels={{
+          left: {
+            title: "NOPE",
+            style: {
+              label: {
+                backgroundColor: "red",
+                borderColor: "black",
+                color: "white",
+                borderWidth: 1,
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "flex-end",
+                justifyContent: "flex-start",
+                marginTop: 30,
+                marginLeft: -30,
+              },
+            },
+          },
+          right: {
+            title: "LIKE",
+            style: {
+              label: {
+                backgroundColor: "green",
+                borderColor: "black",
+                color: "white",
+                borderWidth: 1,
+              },
+              wrapper: {
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "flex-start",
+                marginTop: 30,
+                marginLeft: 30,
+              },
+            },
+          },
+        }}
+        animateOverlayLabelsOpacity
+        animateCardOpacity
+        swipeBackCard
+      />
+      <View style={styles.buttonContainer}>
+        <Button title="Swipe Left" onPress={swipeLeft} />
+        <Button title="Swipe Right" onPress={swipeRight} />
+      </View>
+    </View>
   );
 };
 
 export default SwipeCard;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#F5FCFF',
-    },
-    card: {
-      borderRadius: 10,
-      borderWidth: 2,
-      justifyContent: 'center',
-      backgroundColor: 'white',
-      elevation:10,
-      height:'80%'
-    },
-    text: {
-      textAlign: 'center',
-      fontSize: 50,
-      backgroundColor: 'transparent'
-    },
-    done: {
-      textAlign: 'center',
-      fontSize: 30,
-      color: 'white',
-      backgroundColor: 'transparent'
-    },
-    negative:{
-        color:'red',
-        position:'absolute',
-        top:10,
-        right:10,
-    },
-    positive:{
-        color:'green',
-        position:'absolute',
-        top:10,
-        left:10
-    }
-  })
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF",
+  },
+  card: {
+    borderRadius: 10,
+    borderWidth: 2,
+    justifyContent: "center",
+    backgroundColor: "white",
+    elevation: 10,
+    height: "80%",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
+  },
+  buttonContainer: {
+    alignSelf:'center',
+    position:'absolute',
+    bottom:10,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+});
